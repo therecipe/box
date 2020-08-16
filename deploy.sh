c245bdccb8c0f4bd10709f64f32ec64edb333574
therecipe <therecipe@users.noreply.github.com>
2020-08-16 14:48:53 +0200

    fix CI

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,13 @@ go install -v -tags=no_env github.com/therecipe/qt/cmd/...
 OPWD=$PWD
 export QT_FAT=true
 
-#$(go env GOPATH)/bin/qtdeploy -docker -tags=http_interop build windows_64_static full && docker rmi therecipe/qt:windows_64_static
-#cd $OPWD/full/deploy/windows && zip -9qrXy ../windows_amd64_513_full_http.zip * && cd $OPWD && rm -rf $OPWD/full/deploy/windows
+#TODO: fix QT_FAT for wine
+cmd="echo 'REGEDIT4
+
+[HKEY_CURRENT_USER\Environment]
+\"QT_FAT\"=\"true\"' > env.reg && regedit env.reg && wineserver -w && qtdeploy -tags=http_interop build windows /media/sf_GOPATH0/src/github.com/therecipe/box/full"
+docker run --rm -v $(go env GOPATH):/media/sf_GOPATH0 -e GOPATH=/home/user/work:/media/sf_GOPATH0 -i therecipe/qt:windows_64_shared_wine bash -c "$cmd" && docker rmi therecipe/qt:windows_64_shared_wine
+cd $OPWD/full/deploy && zip -9qrXy windows_amd64_513_full_http.zip windows && cd $OPWD && rm -rf $OPWD/full/deploy/windows
 
 $(go env GOPATH)/bin/qtdeploy -docker -tags=http_interop build linux_static full && docker rmi therecipe/qt:linux_static
 cd $OPWD/full/deploy/linux && zip -9qrXy ../linux_amd64_513_full_http.zip * && cd $OPWD && rm -rf $OPWD/full/deploy/linux
